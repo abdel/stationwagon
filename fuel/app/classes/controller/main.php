8239bf3a90cc55842fe0ea0da9a4483135f4fdc6
Alfie <Alfredo_rivera340@hotmail.com>
2011-01-06 15:44:38 +0000

    Adding the controllers we are going to work with.

---
+++ fuel/app/classes/controller/main.php
@@ -0,0 +1,16 @@
+<?php
+
+class Controller_Main extends Controller_Template {
+
+	public function action_index()
+	{
+		
+	}
+	
+	public function action_404()
+	{
+		
+	}
+}
+
+/* End of file main.php */
---
+++ fuel/app/classes/model/main.php
@@ -0,0 +1,5 @@
+<?php
+
+class Model_Main extends ActiveRecord\Model { }
+
+/* End of file main.php */
