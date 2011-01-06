a5f12e142ad387bb212122fb1ea6032345fd1ebe
Alfie <Alfredo_rivera340@hotmail.com>
2011-01-06 16:59:41 +0000

    Getting the fucking spacing on the wagon controller. Abdel's fault.

--- fuel/app/classes/controller/wagon.php
+++ fuel/app/classes/controller/wagon.php
@@ -1,12 +1,12 @@
 <?php
 
 abstract class Controller_Wagon extends \Controller {
-    
-    protected $data;
-    
-    public function view($view)
-    {
-        $data['yield'] = $this->render($view, $this->data);
-        $this->render('layouts/default', $data);
-    }
+
+	protected $data;
+
+	public function view($view)
+	{
+		$data['yield'] = $this->render($view, $this->data);
+		$this->render('layouts/default', $data);
+	}
 }
