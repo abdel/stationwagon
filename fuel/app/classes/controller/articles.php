578fa6407fa1e1b6f100f46a1e5c5c5472fa6adf
Alfie <Alfredo_rivera340@hotmail.com>
2011-01-06 16:47:47 +0000

    Added the controllers again, git is gay.

---
+++ fuel/app/classes/controller/articles.php
@@ -0,0 +1,8 @@
+<?php
+class Controller_Articles extends Controller_Wagon {
+
+	public function action_index()
+	{
+		
+	}
+}
---
+++ fuel/app/classes/controller/categories.php
@@ -0,0 +1,8 @@
+<?php
+class Controller_Categories extends Controller_Wagon {
+
+	public function action_index()
+	{
+		
+	}
+}
