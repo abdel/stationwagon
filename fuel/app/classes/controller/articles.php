26a70bf2925f23e5dee1d4ed7b293162ad2b7f85
Abdelrahman Mahmoud <abdelm.is@gmail.com>
2011-01-06 22:58:48 +0200

    new changes

--- fuel/app/classes/controller/articles.php
+++ fuel/app/classes/controller/articles.php
@@ -1,19 +1,20 @@
 <?php
 
-class Controller_Main extends Controller_Template {
-
-	/*
-		@access		public
-	*/
-	public function action_index()
-	{
-		
-	}
+class Controller_Articles extends Controller_Wagon {
+    
+    /**
+     * @access public
+     */
+    public function action_index()
+    {
+        $this->data['hello'] = 'Hello world!';
+        $this->view('articles/index');
+    }
 	
-	public function action_404()
-	{
+    public function action_404()
+    {
 		
-	}
+    }
 }
 
-/* End of file main.php */
+/* End of file articles.php */
---
+++ fuel/app/classes/controller/categories.php
@@ -0,0 +1,19 @@
+<?php
+
+class Controller_Categories extends Controller_Wagon {
+    
+    /**
+     * @access public
+     */
+    public function action_index()
+    {
+		
+    }
+	
+    public function action_404()
+    {
+		
+    }
+}
+
+/* End of file articles.php */
