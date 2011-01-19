90fad81eebc8ab0a84d2d4941e9efa3c49ece346
Abdelrahman Mahmoud <abdelm.is@gmail.com>
2011-01-19 22:37:57 +0200

    updated database.sql

--- database.sql
+++ database.sql
@@ -3,18 +3,36 @@ DROP TABLE IF EXISTS `sw_articles`;
 CREATE TABLE `sw_articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category_id` int(11) DEFAULT NULL,
-  `title` varchar(140) DEFAULT NULL,
-  `body` text,
+  `title` varchar(140) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `body` text COLLATE utf8_unicode_ci,
   `created_time` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 DROP TABLE IF EXISTS `sw_categories`;
 
 CREATE TABLE `sw_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(140) DEFAULT NULL,
-  `description` tinytext,
+  `name` varchar(140) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` tinytext COLLATE utf8_unicode_ci,
   `created_time` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `sw_simpleusers`;
+
+CREATE TABLE `sw_simpleusers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `email` varchar(70) DEFAULT NULL,
+  `profile_fields` text,
+  `group` int(11) DEFAULT NULL,
+  `last_login` bigint(20) DEFAULT NULL,
+  `login_hash` tinytext,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
